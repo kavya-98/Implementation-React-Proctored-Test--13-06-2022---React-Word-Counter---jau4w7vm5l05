3fe782e77d7206e3ab3155cef14c09724c0140f3
kavya-98 <76950344+kavya-98@users.noreply.github.com>
2022-06-13 21:00:10 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,39 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+//import '../styles/App.css';
 const App = () => {
-
+  const [text, settext] = useState("");
+  const [max, setmax] = useState(50);
+  const [size, setsize] = useState(0);
+  const [char, setchar] = useState(0);
+  // const font={}
+  const handlechange = (e) => {
+    settext(e.target.value);
+    setchar(text.split(" ").length);
+  };
 
   return (
     <div id="main">
+      <textarea
+        style={{ fontSize: { size } }}
+        maxLength={max}
+        onChange={handlechange}
+      ></textarea>
+      <div className="word-counter">{char}</div>
+      <div id="char-counter">{text.length}</div>
+      <input
+        type="number"
+        value={max}
+        onChange={(e) => setmax(e.target.value)}
+        id="char-limit-input"
+      />
+      <input
+        type="range"
+        max="32px"
+        min="16"
+        onChange={(e) => setsize(e.target.value)}
+      />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
